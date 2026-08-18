0da0a113c6a031c3faebce680c17fc5aa7bb4dcf
AtomNucleus <55904253+AtomNucleus@users.noreply.github.com>
2026-08-18 18:58:45 -0400

    Activate showcase enhancement layer

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './style.css';
 import { Game } from './game/Game.js';
+import { enhanceInstance } from './game/Enhancements.js';
 
 const canvas = document.querySelector('#game');
 const game = new Game(canvas);
+enhanceInstance(game);
 game.start();
 
 window.__MAPLES_GAME__ = game;

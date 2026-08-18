3d1912ec8f81e2003be653b73446025461351c18
AtomNucleus <55904253+AtomNucleus@users.noreply.github.com>
2026-08-18 18:39:49 -0400

    Add game entrypoint

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+import { Game } from './game/Game.js';
+
+const canvas = document.querySelector('#game');
+const game = new Game(canvas);
+game.start();
+
+window.__MAPLES_GAME__ = game;

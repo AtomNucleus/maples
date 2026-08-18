18c816f10c46686bd6626458ac19a2950935d6d6
AtomNucleus <55904253+AtomNucleus@users.noreply.github.com>
2026-08-18 19:16:28 -0400

    Activate imported animated RPG assets

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './style.css';
 import { Game } from './game/Game.js';
 import { enhanceInstance } from './game/Enhancements.js';
+import { installAssetVisuals } from './game/AssetVisuals.js';
 
 const canvas = document.querySelector('#game');
 const game = new Game(canvas);
 enhanceInstance(game);
+installAssetVisuals(game);
 game.start();
 
 window.__MAPLES_GAME__ = game;

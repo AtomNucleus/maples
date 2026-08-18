d7cd99203bef654a1c41201cc2e0cf68fba897cb
AtomNucleus <55904253+AtomNucleus@users.noreply.github.com>
2026-08-18 19:22:58 -0400

    Activate imported environment assets

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './style.css';
 import { Game } from './game/Game.js';
 import { enhanceInstance } from './game/Enhancements.js';
 import { installAssetVisuals } from './game/AssetVisuals.js';
+import { installEnvironmentAssets } from './game/EnvironmentAssets.js';
 
 const canvas = document.querySelector('#game');
 const game = new Game(canvas);
 enhanceInstance(game);
 installAssetVisuals(game);
+installEnvironmentAssets(game);
 game.start();
 
 window.__MAPLES_GAME__ = game;
